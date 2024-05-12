802d2745545de7d35fbafad023cf184010d10cbf
天青色等烟雨 <2065727482@qq.com>
2024-05-12 21:39:56 +0800

    1

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, send_from_directory, abort,render_template
 from datetime import datetime  # 导入datetime模块
+
 
 app = Flask(__name__)
 
 @app.route("/")
+def text():
+    return render_template('example.html')  # 假设你的 HTML 文件叫
 def index():
     current_time = datetime.now()  # 获取当前时间
+    image_dir = ''
     return current_time.strftime('%Y-%m-%d %H:%M:%S')  # 格式化时间并返回
+# @app.route('/show-image/<path:filename>')
+# def show_image(filename):
+#     image_dir = 'D:\Desktop\备份'  # 替换为你的图片目录路径
+#     if not filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
+#         abort(404)
+#     return send_from_directory(image_dir, filename)
 if __name__ == "__main__":
     app.run(debug=True)  # 添加这一行来启动Flask应用
--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,6 @@ import os
 from dotenv import load_dotenv
 dotenv_path = os.path.join(os.path.dirname(__file__),'env')
 if os.path.exists(dotenv_path):
-    load_dotenv(dotenv.path)
-from app import index
+    load_dotenv(dotenv_path)
+from app import app
+application = app
