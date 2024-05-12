2a818937a524b51b881043793cdb6ef2df4fba21
天青色等烟雨 <2065727482@qq.com>
2024-05-12 19:27:41 +0800

    1

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from datetime import datetime  # 导入datetime模块
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    current_time = datetime.now()  # 获取当前时间
+    return current_time.strftime('%Y-%m-%d %H:%M:%S')  # 格式化时间并返回
+if __name__ == "__main__":
+    app.run(debug=True)  # 添加这一行来启动Flask应用
